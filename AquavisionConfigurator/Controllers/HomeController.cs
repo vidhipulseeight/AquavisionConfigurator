f6ec3851907df5e05cf92c2894cdadb0bfb042d6
VidhiTrivedi <vidhi.trivedi@PULSE-EIGHT.NET>
2023-12-13 15:33:40 +0000

    Add project files.

---
+++ AquavisionConfigurator/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using AquavisionConfigurator.Models;
+using System.Linq;
+using System.Web.Mvc;
+namespace AquavisionConfigurator.Controllers {
+	public class HomeController : Controller {
+		AquavisionEntities myDB = new AquavisionEntities();
+		public ActionResult Index() {
+			ViewBag.Category = myDB.Categories.ToList();
+			return View();
+		}
+		public ActionResult Shop(int? productId) {
+			ViewBag.Products = myDB.Products.ToList();
+			if (productId == null) {
+				var defaultProduct = myDB.Products.FirstOrDefault();
+				if (defaultProduct != null) {
+					productId = defaultProduct.Id;
+				}
+			}
+			ViewBag.ProductOptionGroups = myDB.ProductOptionGroups.Where(p => p.ProductId == productId).ToList();
+			return View();
+		}
+	}
+}
---
+++ AquavisionConfigurator/aquavision/js/custom.js
@@ -0,0 +1,18 @@
+﻿var canvas = document.getElementById('can');
+var c = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var canvas_stack = new CanvasStack('can');
+var layers1 = canvas_stack.createLayer();
+var layers1_ctx = document.getElementById(layers1).getContext('2d');
+
+var layers2 = canvas_stack.createLayer();
+var layers2_ctx = document.getElementById(layers2).getContext('2d');
+
+layers1_ctx.fillRect(0, 0, 100, 100);
+
+layers2_ctx.fillStyle = 'red';
+layers2_ctx.fillRect(100, 100, 100, 100);
+
+layers1_ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
